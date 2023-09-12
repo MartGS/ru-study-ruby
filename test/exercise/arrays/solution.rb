7c8728cb60d5cf1b33c7fb20403f583f50b5ba4f
Григорий Мартынов <grigorii@MacBook-Pro-GS.local>
2023-09-12 15:49:16 +0300

    Update code after comments George Yakimov

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,21 +2,16 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array.map { |e| e.positive? ? 100 : e }
+        max_number = array.reduce { |x, n| x = n if n > x; x }
+        array.map { |e| e.positive? ? max_number : e }
       end
 
       def search(array, query, low = 0, high = array.size - 1)
-        return -1 if low > high
-
+        return -1 if !array.include?(query)
+        
         mid = (low + high) / 2
-
-        if query == array[mid]
-          mid
-        elsif query < array[mid]
-          search(array, query, low, mid - 1)
-        else
-          search(array, query, mid + 1, high)
-        end
+        
+        query == array[mid] ? mid : query < array[mid] ? search(array, query, low, mid - 1) : search(array, query, mid + 1, high)
       end
     end
   end
