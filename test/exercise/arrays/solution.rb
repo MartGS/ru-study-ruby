88f21a71bdeb6351ba18f0b096463a7c3485b8a0
Григорий Мартынов <grigorii@MacBook-Pro-GS.local>
2023-09-15 15:57:33 +0300

    Solution for Arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -7,11 +7,23 @@ module Exercise
       end
 
       def search(array, query, low = 0, high = array.size - 1)
-        return -1 if !array.include?(query)
+        return -1 if array.empty? || array_not_include_query?(array, query) || subarray_not_exist?(low, high)
         
         mid = (low + high) / 2
         
-        query == array[mid] ? mid : query < array[mid] ? search(array, query, low, mid - 1) : search(array, query, mid + 1, high)
+        return mid if query == array[mid]
+
+        query < array[mid] ? search(array, query, low, mid - 1) : search(array, query, mid + 1, high)
+      end
+
+      private
+
+      def array_not_include_query?(array, query)
+        query < array[0] || query > array[-1]
+      end
+
+      def subarray_not_exist?(low, high)
+        low > high
       end
     end
   end
