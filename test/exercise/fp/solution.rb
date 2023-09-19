31ea8c1752bb554070f4f0990057e863065f2c92
Григорий Мартынов <grigorii@MacBook-Pro-GS.local>
2023-09-19 13:12:20 +0300

    Update Map/Reduce solutions after mentor's comments

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,51 +5,42 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        @count_film = 0
-        @total_raiting = 0
-
-        array.map do |film|
-          calculating_values_for_result(film) if is_film_match_search_criteria?(film)
+        new_array = array.map do |film|
+          film["rating_kinopoisk"].to_f if is_film_match_search_criteria?(film)
         end
+        
+        new_array.compact!
 
-        result = @total_raiting / @count_film
+        result = new_array.sum / new_array.size
       end
       
       def chars_count(films, threshold)
-        @count_letter_i = 0
-
-        films.map do |film|
-          count_letter_i(film) if is_film_match_raiting_value?(film, threshold)
+        letter_i = films.reduce(0) do |x, film|
+          x += film["name"].count "и" if is_film_match_raiting_value?(film, threshold)
+          x
         end
 
-        @count_letter_i
+        letter_i
       end
 
       private
-
-      def count_letter_i(film)
-        @count_letter_i += film["name"].count "и"
-      end
 
       def is_film_match_raiting_value?(film, threshold)
         film["rating_kinopoisk"].to_f >= threshold
       end
 
-      def calculating_values_for_result(film)
-        @total_raiting += film["rating_kinopoisk"].to_f
-        @count_film += 1
-      end
-
       def is_film_match_search_criteria?(film)
-        is_multiple_countries?(film)
+        return false unless is_multiple_countries?(film)
+        return false unless is_rating_kinopoisk?(film)
+        true
       end
 
       def is_multiple_countries?(film)
-        film["country"].present? && film["country"].split(',').size > 1 ? is_rating_kinopoisk?(film) : false
+        film["country"].present? && film["country"].split(',').size > 1
       end
 
       def is_rating_kinopoisk?(film)
-        film["rating_kinopoisk"].present? && film["rating_kinopoisk"].to_f > 0 ? true : false
+        film["rating_kinopoisk"].present? && film["rating_kinopoisk"].to_f > 0
       end
     end
   end
