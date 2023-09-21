843d72a15101c106648f9de27b52f522fbbbd999
Григорий Мартынов <grigorii@MacBook-Pro-GS.local>
2023-09-21 12:10:22 +0300

    Update solution

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,23 +5,23 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def rating(array)
-        new_array = array.map do |film|
+        filtered_array = array.map do |film|
           film["rating_kinopoisk"].to_f if is_film_match_search_criteria?(film)
         end
         
-        new_array.compact!
-        total_raiting = new_array.inject(0, :+)
+        filtered_array.compact!
+        total_raiting = filtered_array.inject(0, :+)
 
-        result = total_raiting / new_array.size
+        p result = total_raiting / filtered_array.size
       end
       
       def chars_count(films, threshold)
-        letter_i = films.reduce(0) do |x, film|
-          x += film["name"].count "и" if is_film_match_raiting_value?(film, threshold)
-          x
+        total_letter_i = films.reduce(0) do |acc, film|
+          acc += film["name"].count "и" if is_film_match_raiting_value?(film, threshold)
+          acc
         end
 
-        letter_i
+        p total_letter_i
       end
 
       private
@@ -31,17 +31,7 @@ module Exercise
       end
 
       def is_film_match_search_criteria?(film)
-        return false unless is_multiple_countries?(film)
-        return false unless is_rating_kinopoisk?(film)
-        true
-      end
-
-      def is_multiple_countries?(film)
-        film["country"].present? && film["country"].split(',').size > 1
-      end
-
-      def is_rating_kinopoisk?(film)
-        film["rating_kinopoisk"].present? && film["rating_kinopoisk"].to_f > 0
+        film["country"].present? && film["country"].split(',').size > 1 && film["rating_kinopoisk"].present? && film["rating_kinopoisk"].to_f > 0
       end
     end
   end
